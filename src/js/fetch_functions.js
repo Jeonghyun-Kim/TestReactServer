56f6e4377251ee140604f0b6fa8b5b094256839b
Kay <kimjh@bawi.org>
2020-05-30 19:16:40 +0900

    modulize fetch

--- src/js/fetch_functions.js
+++ src/js/fetch_functions.js
@@ -1,82 +1,44 @@
 import { renewToken } from './auth_utils';
 import { SERVER_URL, KEY, ERROR_CODE } from './defines';
 
-const getMyInfo = async (cb) => {
+const fetchServer = async (url, method, body, cb) => {
   const token = sessionStorage.getItem(KEY.ACCESS_TOKEN);
   try {
-    const response = await fetch(`${SERVER_URL}/my`, {
-      method: 'GET',
+    const response = await fetch(`${SERVER_URL}${url}`, {
+      method,
       headers: {
         'Content-Type': 'application/json',
         Authorization: token,
+        body,
       },
     });
     const resJson = await response.json();
     if (resJson.error === ERROR_CODE.TOKEN_EXPIRED) {
       renewToken((flag) => {
         if (flag) {
-          getMyInfo(cb);
+          fetchServer(url, method, body, cb);
         } else {
           cb({ error: ERROR_CODE.API_SERVER_DOWN });
         }
       });
+    } else {
+      cb(resJson);
     }
-    cb(resJson);
   } catch (error) {
     cb({ error: ERROR_CODE.API_SERVER_DOWN });
   }
+};
+
+const getMyInfo = (cb) => {
+  fetchServer('/my', 'GET', null, cb);
 };
 
 const postPainting = async (formData, cb) => {
-  const token = sessionStorage.getItem(KEY.ACCESS_TOKEN);
-  try {
-    const response = await fetch(`${SERVER_URL}/painting`, {
-      method: 'POST',
-      headers: {
-        Authorization: token,
-      },
-      body: formData,
-    });
-    const resJson = await response.json();
-    if (resJson.error === ERROR_CODE.TOKEN_EXPIRED) {
-      renewToken((flag) => {
-        if (flag) {
-          postPainting(cb);
-        } else {
-          cb({ error: ERROR_CODE.API_SERVER_DOWN });
-        }
-      });
-    }
-    cb(resJson);
-  } catch (error) {
-    cb({ error: ERROR_CODE.API_SERVER_DOWN });
-  }
+  fetchServer('/painting', 'POST', formData, cb);
 };
 
 const getPainting = async (id, cb) => {
-  const token = sessionStorage.getItem(KEY.ACCESS_TOKEN);
-  try {
-    const response = await fetch(`${SERVER_URL}/painting/${id}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: token,
-      },
-    });
-    const resJson = await response.json();
-    if (resJson.error === ERROR_CODE.TOKEN_EXPIRED) {
-      renewToken((flag) => {
-        if (flag) {
-          getPainting(id, cb);
-        } else {
-          cb({ error: ERROR_CODE.API_SERVER_DOWN });
-        }
-      });
-    }
-    cb(resJson);
-  } catch (error) {
-    cb({ error: ERROR_CODE.API_SERVER_DOWN });
-  }
+  fetchServer(`/painting/${id}`, 'GET', null, cb);
 };
 
 export {
